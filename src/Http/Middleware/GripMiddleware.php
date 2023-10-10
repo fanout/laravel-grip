5e6405f8a75c59e530a0db334684cf4c913327c9
Madeline Boby <madeline.boby@fastly.com>
2023-10-10 11:17:43 -0400

    Support verify-iss

--- src/Http/Middleware/GripMiddleware.php
+++ src/Http/Middleware/GripMiddleware.php
@@ -98,7 +98,7 @@ class GripMiddleware {
             // If every client needs signing, then we mark as requires_signed;
             $requires_signed = true;
             foreach( $clients as $client ) {
-                if( !($client->auth instanceof JwtAuth && !empty($client->auth->key)) ) {
+                if( empty($client->get_verify_key()) ) {
                     $requires_signed = false;
                     break;
                 }
@@ -114,7 +114,7 @@ class GripMiddleware {
                     /** @var JwtAuth $auth */
                     $auth = $client->auth;
                     Log::debug('validating: ' . $grip_sig . ' with ' . $auth->key );
-                    if( JwtAuth::validate_signature( $grip_sig, $auth->key ) ) {
+                    if( JwtAuth::validate_signature( $grip_sig, $client->get_verify_key(), $client->get_verify_iss() ) ) {
                         Log::debug('validated' );
                         $is_signed = true;
                         break;
